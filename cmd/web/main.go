ac3acda4d7e8bdc9c84754c3d06be1b20f670c68
selim <salimmia3745@gmail.com>
2023-05-30 15:56:02 +0600

    Setting application wide configuration is added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/salimmia/go-course/pkg/config"
 	"github.com/salimmia/go-course/pkg/handlers"
+	"github.com/salimmia/go-course/pkg/render"
 	// "os"
 )
 
@@ -12,6 +15,16 @@ const portNumber = ":8080"
 
 // main is the main application function
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+
 	http.HandleFunc("/home", handlers.Home)
 
 	http.HandleFunc("/about", handlers.About)
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -11,7 +11,7 @@ import (
 // RenderTemplate renders a template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	// create a render template cache
-	tc, err := createTemplateCache()
+	tc, err := CreateTemplateCache()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,7 +40,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	}
 }
 
-func createTemplateCache() (map[string]*template.Template, error) {
+func CreateTemplateCache() (map[string]*template.Template, error) {
 	myCache := map[string]*template.Template{}
 
 	pages, err := filepath.Glob("./templates/*.page.html")
