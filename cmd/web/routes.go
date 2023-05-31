a1fd208fc8532973092ff9c4e17afea7610ae445
selim <salimmia3745@gmail.com>
2023-05-31 18:06:28 +0600

    Using chi for routing is added

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,21 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
 	"github.com/salimmia/go-course/pkg/config"
 	"github.com/salimmia/go-course/pkg/handlers"
 )
 
-func routes(app *config.AppConfig) http.Handler{
-	mux := pat.New()
-	
-	mux.Get("/home", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-	
+func routes(app *config.AppConfig) http.Handler {
+	// mux := pat.New()
+
+	// mux.Get("/home", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+	mux := chi.NewRouter()
+
+	mux.Get("/home", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
 	return mux
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/salimmia/go-course
 
 go 1.18
 
-require github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f // indirect
+require github.com/go-chi/chi/v5 v5.0.8
